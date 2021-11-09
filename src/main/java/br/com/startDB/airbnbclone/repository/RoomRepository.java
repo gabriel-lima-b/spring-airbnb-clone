f73d961ed532b0084a5b5aa71286cf5754ade09e
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-11-09 17:26:18 -0300

    #3 Created RoomRepository

---
+++ src/main/java/br/com/startDB/airbnbclone/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package br.com.startDB.airbnbclone.repository;
+
+import java.util.UUID;
+import org.springframework.data.repository.CrudRepository;
+import br.com.startDB.airbnbclone.model.Room;
+
+public interface RoomRepository extends CrudRepository<Room, UUID>{
+
+}
