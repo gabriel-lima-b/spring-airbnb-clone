c5bcc2ce6d579101e928a4299105c2f51a7b81f2
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-11-09 17:41:53 -0300

    #4 Created interface AirbnbService

---
+++ src/main/java/br/com/startDB/airbnbclone/service/AirbnbService.java
@@ -0,0 +1,32 @@
+package br.com.startDB.airbnbclone.service;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import org.springframework.dao.DataAccessException;
+
+import br.com.startDB.airbnbclone.model.Guest;
+import br.com.startDB.airbnbclone.model.Host;
+import br.com.startDB.airbnbclone.model.Room;
+
+public interface AirbnbService {
+
+	Guest findGuestById(UUID id) throws DataAccessException;
+	Collection<Guest> findAllGuests() throws DataAccessException;
+	void saveGuest(Guest guest) throws DataAccessException;
+	void deleteGuest(Guest guest) throws DataAccessException;
+	
+	Host findHostById(UUID id) throws DataAccessException;
+	Collection<Host> findAllHosts() throws DataAccessException;
+	void saveHost(Host host) throws DataAccessException;
+	void deleteHost(Host host) throws DataAccessException;
+
+	Room findPetById(UUID id) throws DataAccessException;
+	Collection<Room> findAllRooms() throws DataAccessException;
+	void saveRoom(Room room) throws DataAccessException;
+	void deleteRoom(Room room) throws DataAccessException;
+
+	
+	
+
+}
