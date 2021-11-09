4459b8eb2ebf5b005c3cff862c5acc69b591c4f0
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-11-09 18:14:54 -0300

    #4 Created AirbnbServiceImpl

--- src/main/java/br/com/startDB/airbnbclone/service/AirbnbService.java
+++ src/main/java/br/com/startDB/airbnbclone/service/AirbnbService.java
@@ -12,17 +12,17 @@ import br.com.startDB.airbnbclone.model.Room;
 public interface AirbnbService {
 
 	Guest findGuestById(UUID id) throws DataAccessException;
-	Collection<Guest> findAllGuests() throws DataAccessException;
+	Iterable<Guest> findAllGuests() throws DataAccessException;
 	void saveGuest(Guest guest) throws DataAccessException;
 	void deleteGuest(Guest guest) throws DataAccessException;
 	
 	Host findHostById(UUID id) throws DataAccessException;
-	Collection<Host> findAllHosts() throws DataAccessException;
+	Iterable<Host> findAllHosts() throws DataAccessException;
 	void saveHost(Host host) throws DataAccessException;
 	void deleteHost(Host host) throws DataAccessException;
 
 	Room findPetById(UUID id) throws DataAccessException;
-	Collection<Room> findAllRooms() throws DataAccessException;
+	Iterable<Room> findAllRooms() throws DataAccessException;
 	void saveRoom(Room room) throws DataAccessException;
 	void deleteRoom(Room room) throws DataAccessException;
 
---
+++ src/main/java/br/com/startDB/airbnbclone/service/AirbnbServiceImpl.java
@@ -0,0 +1,104 @@
+package br.com.startDB.airbnbclone.service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import br.com.startDB.airbnbclone.model.Guest;
+import br.com.startDB.airbnbclone.model.Host;
+import br.com.startDB.airbnbclone.model.Room;
+import br.com.startDB.airbnbclone.repository.GuestRepository;
+import br.com.startDB.airbnbclone.repository.HostRepository;
+import br.com.startDB.airbnbclone.repository.RoomRepository;
+
+@Service
+public class AirbnbServiceImpl implements AirbnbService {
+
+	private GuestRepository guestRepository;
+	private HostRepository hostRepository;
+	private RoomRepository roomRepository;
+
+	@Autowired
+	public AirbnbServiceImpl(GuestRepository guestRepository, HostRepository hostRepository,
+			RoomRepository roomRepository) {
+		this.guestRepository = guestRepository;
+		this.hostRepository = hostRepository;
+		this.roomRepository = roomRepository;
+	}
+
+	@Override
+	public Guest findGuestById(UUID id) throws DataAccessException {
+		Optional<Guest> guest = guestRepository.findById(id);
+		if (guest.isEmpty()) {
+			return null;
+		}
+		return guest.get();
+	}
+
+	@Override
+	public Iterable<Guest> findAllGuests() throws DataAccessException {
+		return guestRepository.findAll();
+	}
+
+	@Override
+	public void saveGuest(Guest guest) throws DataAccessException {
+		guestRepository.save(guest);
+	}
+
+	@Override
+	public void deleteGuest(Guest guest) throws DataAccessException {
+		guestRepository.delete(guest);
+	}
+
+	@Override
+	public Host findHostById(UUID id) throws DataAccessException {
+		Optional<Host> host = hostRepository.findById(id);
+		if (host.isEmpty()) {
+			return null;
+		}
+		return host.get();
+	}
+
+	@Override
+	public Iterable<Host> findAllHosts() throws DataAccessException {
+		return hostRepository.findAll();
+	}
+
+	@Override
+	public void saveHost(Host host) throws DataAccessException {
+		hostRepository.save(host);
+	}
+
+	@Override
+	public void deleteHost(Host host) throws DataAccessException {
+		hostRepository.delete(host);
+	}
+
+	@Override
+	public Room findPetById(UUID id) throws DataAccessException {
+		Optional<Room> room = roomRepository.findById(id);
+		if (room.isEmpty()) {
+			return null;
+		}
+		return room.get();
+	}
+
+	@Override
+	public Iterable<Room> findAllRooms() throws DataAccessException {
+		return roomRepository.findAll();
+	}
+
+	@Override
+	public void saveRoom(Room room) throws DataAccessException {
+		roomRepository.save(room);
+	}
+
+	@Override
+	public void deleteRoom(Room room) throws DataAccessException {
+		roomRepository.delete(room);
+	}
+
+}
