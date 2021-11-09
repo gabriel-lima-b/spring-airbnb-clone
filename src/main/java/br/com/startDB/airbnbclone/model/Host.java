d8dcee05352048ac3c2385c3f91e8fef4593d63d
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-11-09 18:20:04 -0300

    Removed unused imports

--- src/main/java/br/com/startDB/airbnbclone/model/Host.java
+++ src/main/java/br/com/startDB/airbnbclone/model/Host.java
@@ -1,15 +1,11 @@
 package br.com.startDB.airbnbclone.model;
 
 import java.util.Set;
-import java.util.UUID;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 import javax.persistence.OneToMany;
 
 @Entity
