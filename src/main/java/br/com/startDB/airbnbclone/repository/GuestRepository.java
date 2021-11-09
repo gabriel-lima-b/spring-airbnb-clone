095cb92d9c565348d38b52141e93e6826d47cb26
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-11-09 17:27:51 -0300

    #3 Created GuestRepository

---
+++ src/main/java/br/com/startDB/airbnbclone/repository/GuestRepository.java
@@ -0,0 +1,11 @@
+package br.com.startDB.airbnbclone.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.startDB.airbnbclone.model.Guest;
+
+public interface GuestRepository extends CrudRepository<Guest, UUID>{
+
+}
