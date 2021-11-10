d766bde8601da9b0e7aa760af6716254dc979862
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-11-10 18:35:14 -0300

    #6 Created JacksonCustomHostSerializer and JacksonCustomHostDeserializer

---
+++ src/main/java/br/com/startDB/airbnbclone/controller/JacksonCustomHostDeserializer.java
@@ -0,0 +1,46 @@
+
+package br.com.startDB.airbnbclone.controller;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import br.com.startDB.airbnbclone.model.Host;
+
+
+public class JacksonCustomHostDeserializer extends StdDeserializer<Host> {
+
+	public JacksonCustomHostDeserializer(){
+		this(null);
+	}
+
+	public JacksonCustomHostDeserializer(Class<Host> t) {
+		super(t);
+	}
+
+	@Override
+	public Host deserialize(JsonParser parser, DeserializationContext context) throws IOException, JsonProcessingException {
+		JsonNode node = parser.getCodec().readTree(parser);
+		Host host= new Host();
+		String name = node.get("name").asText(null);
+		String lastName = node.get("lastName").asText(null);
+		String email = node.get("email").asText(null);
+		String password = node.get("password").asText(null);
+		String phone = node.get("phone").asText(null);
+		if (node.hasNonNull("id")) {
+			host.setId(UUID.fromString(node.get("id").asText(null)));
+		}
+        host.setName(name);
+        host.setLastName(lastName);
+        host.setEmail(email);
+        host.setPassword(password);
+        host.setPhone(phone);
+		return host;
+	}
+
+}
---
+++ src/main/java/br/com/startDB/airbnbclone/controller/JacksonCustomHostSerializer.java
@@ -0,0 +1,59 @@
+
+package br.com.startDB.airbnbclone.controller;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import br.com.startDB.airbnbclone.model.Host;
+import br.com.startDB.airbnbclone.model.Room;
+
+
+public class JacksonCustomHostSerializer extends StdSerializer<Host> {
+
+	public JacksonCustomHostSerializer() {
+		this(null);
+	}
+
+	protected JacksonCustomHostSerializer(Class<Host> t) {
+		super(t);
+	}
+
+	@Override
+	public void serialize(Host host, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+		jgen.writeStartObject(); // host
+		if (host.getId() == null) {
+			jgen.writeNullField("id");
+		} else {
+			jgen.writeStringField("id", host.getId().toString());
+		}
+		jgen.writeStringField("name", host.getName());
+		jgen.writeStringField("lastName", host.getLastName());
+		jgen.writeStringField("email" , host.getEmail());
+		jgen.writeStringField("password" , host.getPassword());
+		jgen.writeStringField("phone" , host.getPhone());
+		
+		jgen.writeArrayFieldStart("rooms");
+		for (Room room : host.getRooms()) {
+			if (room.getId() == null) {
+				jgen.writeNullField("id");
+			} else {
+				jgen.writeStringField("id", room.getId().toString());
+			}
+			jgen.writeStringField( "title", room.getTitle());
+			jgen.writeStringField( "description", room.getDescription());
+			if (room.getHost().getId() == null) {
+				jgen.writeNullField("id");
+			} else {
+				jgen.writeStringField("id", room.getHost().getId().toString());
+			}
+			jgen.writeNumberField("price", room.getPrice());
+			jgen.writeEndObject(); // room
+		}
+		jgen.writeEndArray(); // rooms
+		jgen.writeEndObject(); // host
+	}
+
+}
