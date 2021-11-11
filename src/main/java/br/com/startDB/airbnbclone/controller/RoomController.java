080f930260b70871045284517376d4532ae29533
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-11-11 17:58:40 -0300

    9 Created deleteById

--- src/main/java/br/com/startDB/airbnbclone/controller/RoomController.java
+++ src/main/java/br/com/startDB/airbnbclone/controller/RoomController.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 
 import javax.transaction.Transactional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,47 +25,54 @@ import br.com.startDB.airbnbclone.service.AirbnbService;
 @RequestMapping("api/rooms")
 public class RoomController {
 
-	
-		@Autowired
-		private AirbnbService airbnbService;
-		
-		@RequestMapping(value = "/{roomId}", method = RequestMethod.GET, produces = "application/json")
-		public ResponseEntity<Room> getRoom(@PathVariable("roomId") UUID roomId){
-			Room room = this.airbnbService.findRoomById(roomId);
-			if(room == null){
-				return new ResponseEntity<Room>(HttpStatus.NOT_FOUND);
-			}
-			return new ResponseEntity<Room>(room, HttpStatus.OK);
+	@Autowired
+	private AirbnbService airbnbService;
+
+	@RequestMapping(value = "/{roomId}", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Room> getRoom(@PathVariable("roomId") UUID roomId) {
+		Room room = this.airbnbService.findRoomById(roomId);
+		if (room == null) {
+			return new ResponseEntity<Room>(HttpStatus.NOT_FOUND);
 		}
-		
-		@RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
-		public ResponseEntity<Collection<Room>> getRooms(){
-			List<Room> rooms = (List<Room>) this.airbnbService.findAllRooms();
-			if(rooms.isEmpty()){
-				return new ResponseEntity<Collection<Room>>(HttpStatus.NOT_FOUND);
-			}
-			return new ResponseEntity<Collection<Room>>(rooms, HttpStatus.OK);
-		}
-		
-		@RequestMapping(value = "/city/{city}", method = RequestMethod.GET, produces = "application/json")
-		public ResponseEntity<Collection<Room>> getRoomsByCity(@PathVariable("city") String city){
-			List<Room> rooms = (List<Room>)this.airbnbService.findAllRoomsByCity(city);
-			if(rooms.isEmpty()){
-				return new ResponseEntity<Collection<Room>>(HttpStatus.NOT_FOUND);
-			}
-			return new ResponseEntity<Collection<Room>>(rooms, HttpStatus.OK);
-		}
-		
-		@RequestMapping(method = RequestMethod.POST, produces = "application/json")
-		public ResponseEntity<Room> save(@RequestBody Room room, BindingResult bindingResult) {
-			if(bindingResult.hasErrors() || (room == null)){
-				return new ResponseEntity<Room>(room, HttpStatus.BAD_REQUEST);
-			}
-			airbnbService.saveRoom(room);
-			return new ResponseEntity<Room>(room, HttpStatus.CREATED); 
-		}
-		
-		
+		return new ResponseEntity<Room>(room, HttpStatus.OK);
 	}
 
+	@RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Collection<Room>> getRooms() {
+		List<Room> rooms = (List<Room>) this.airbnbService.findAllRooms();
+		if (rooms.isEmpty()) {
+			return new ResponseEntity<Collection<Room>>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Collection<Room>>(rooms, HttpStatus.OK);
+	}
 
+	@RequestMapping(value = "/city/{city}", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Collection<Room>> getRoomsByCity(@PathVariable("city") String city) {
+		List<Room> rooms = (List<Room>) this.airbnbService.findAllRoomsByCity(city);
+		if (rooms.isEmpty()) {
+			return new ResponseEntity<Collection<Room>>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Collection<Room>>(rooms, HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<Room> save(@RequestBody Room room, BindingResult bindingResult) {
+		if (bindingResult.hasErrors() || (room == null)) {
+			return new ResponseEntity<Room>(room, HttpStatus.BAD_REQUEST);
+		}
+		airbnbService.saveRoom(room);
+		return new ResponseEntity<Room>(room, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/{roomId}", method = RequestMethod.DELETE, produces = "application/json")
+	public ResponseEntity<Room> deleteById(@PathVariable("roomId") UUID roomId) {
+		Room room = this.airbnbService.findRoomById(roomId);
+		if (room == null) {
+			return new ResponseEntity<Room>(room, HttpStatus.NOT_FOUND);
+		}
+		room.getHost().removeRoom(room);
+		airbnbService.deleteRoom(room);
+		return new ResponseEntity<Room>(room, HttpStatus.OK);
+	}
+
+}
