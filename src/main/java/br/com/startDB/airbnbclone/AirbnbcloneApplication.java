9ccc3d0dc1a66cf6875621c2174d0c33842a6e81
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-11-12 11:00:12 -0300

    Testing database

--- src/main/java/br/com/startDB/airbnbclone/AirbnbcloneApplication.java
+++ src/main/java/br/com/startDB/airbnbclone/AirbnbcloneApplication.java
@@ -21,18 +21,24 @@ public class AirbnbcloneApplication {
 
 	  @Bean
 	  public CommandLineRunner demo(AirbnbService airbnbService) {
-	    return (args) -> {     
-	    	Host h1 = new Host( "exemplo@gmail.com", "senha","nome","sobrenome", "telefone");
-	      airbnbService.saveHost(h1);
+	    return (args) -> {  
+	    	Host h1 = new Host( "febonadio@gmail.com", "senha","Felipe","Bonadio", "5514991518232");
+	    	airbnbService.saveHost(h1);
+	    	
+	    	Host h2 = new Host( "bisaka@gmail.com", "senha","Bianca","Sakaniwa", "5514991515687");
+	    	airbnbService.saveHost(h2);
 	      
-	      Room r1 = new Room( "casa", "casa bonita e azul", h1, new BigDecimal("231"), "London");
-	      airbnbService.saveRoom(r1);
-	      h1.addRoom(r1);
-	      Room r2 = new Room( "casa2", "casa bonita e amarela", h1, new BigDecimal("235"), "Cambridge");
-	      airbnbService.saveRoom(r2);
-	      h1.addRoom(r2);
+	    	Room r1 = new Room( "Casa", "Localizada na rua Emilio Bressan, a casa é espaçosa, bem iluminada "
+	    			+ "e confortável, passando uma sensação de familiaridade ", h1, new BigDecimal("123"), "Barra Bonita");
+	    	airbnbService.saveRoom(r1);
+	    	h1.addRoom(r1);
+	    	
+	    	Room r2 = new Room( "Hotel", "Com quartos grande e equipados com tecnologia de ponta, nosso hotel"
+	    			+ " tem as melhores ofertas para você e sua família!", h2, new BigDecimal("180"), "São Paulo");
+	    	airbnbService.saveRoom(r2);
+	    	h2.addRoom(r2);
 	      
-	      airbnbService.saveGuest(new Guest("guestmail@mail.com","senha","Guest","Exemplo","telefone do guest"));
+	    	airbnbService.saveGuest(new Guest("gabriellima@mail.com","senha","Gabriel","Lima","55519948-6885"));
 	    };
 	  }
 }
